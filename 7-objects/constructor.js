b2c6b1496a3f428c8c6b746dfff721664c504ae7
Catherine J <cjarocki@gmail.com>
2017-02-13 14:18:12 -0600

    Update constructor.js

--- 7-objects/constructor.js
+++ 7-objects/constructor.js
@@ -7,6 +7,7 @@
 // - area() returns the area of this triangle
 // - perimeter() returns the perimeter of this triangle
 
+function RightTriangle(side1, side2, base){
 var side1 = 3;
 var side2 = 4;
 var base = 5;
@@ -19,12 +20,18 @@ function perimeter(side1, side2, base){
 var newPerimeter = (side1 + side2 + base);
 return newPerimeter;
 }
+}
 // 2 - Create two instances of the RightTriangle
 // object. Console log the results of calling
 // both functions on both instances.
 
-function area() {}
-function perimeter() {} 
+function RightTriangle(side1, side2, base){
+ this.side1 = 3;
+ this.side2 = 4;
+ this.base = 5;
+};
+
+var newVar = new RightTriangle(side1, side2, base);
 
 console.log();
 
