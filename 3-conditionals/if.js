bb819443349c411ac15c842fa357dfca7a900c6b
Catherine J <cjarocki@gmail.com>
2017-02-13 13:26:25 -0600

    Update if.js

--- 3-conditionals/if.js
+++ 3-conditionals/if.js
@@ -4,14 +4,22 @@
 var first = 3;
 var second = 5;
 
+if(first = second){
+  console.log("Hello");
+} else if(first != second) {
+  console.log("Goodbye")
+}
+
 // 2 - Expand your answer above to
 // console log "Goodbye" if
 // the variables are not equal
 
-// 3 - Write a statement that
-// assigns the value of the addition
-// of the first & second variables to a new
-// variable called third IF first is greater
-// than second. If second is greater than first
-// assign the difference of the first & second
-// variables to third.
+// 3 - Write a statement that assigns the value of the addition of the first & second variables 
+// to a new variable called third IF first is greater than second. If second is greater than first
+// assign the difference of the first & second variables to third.
+
+if(first > second){
+  var third = first + second;
+} else if (first < second) {
+  var third = second - first;
+}
