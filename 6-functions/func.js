fd52d80dd92ba471324fc76417a5d44da5f120b6
Catherine J <cjarocki@gmail.com>
2017-02-13 13:52:56 -0600

    Update func.js

--- 6-functions/func.js
+++ 6-functions/func.js
@@ -2,12 +2,28 @@
 // that takes no arguments and
 // returns the value 4
 
+var newFunc();
+
+function newFunc(){
+  return 4;
+}
+
 // 2 - Write a function declaration
 // that takes two arguments and
 // returns the value of the result
 // of the multiplication of both
 // arguments
 
+var newerFunc(3, 4);
+
+function newerFunc(a, b){
+  return(a * b);
+}
+
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
+
+function newestFunc(a) {
+  console.log(newestFunc(a));
+}
