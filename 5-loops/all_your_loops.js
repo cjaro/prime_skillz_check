a1b67a4d9e415588005f14412c6e022d198c676d
Catherine J <cjarocki@gmail.com>
2017-02-13 13:44:55 -0600

    Update all_your_loops.js

--- 5-loops/all_your_loops.js
+++ 5-loops/all_your_loops.js
@@ -1,19 +1,26 @@
-// 1 - Write a for loop that runs 5 times
-// Each iteration will increase the value
-// of checkz by 1
+// 1 - Write a for loop that runs 5 times. Each iteration will increase the value of checkz by 1
 var checkz = 0;
 
+for (var i = 0; i < 5; i++) {
+  checkz += 1;
+}
 
-// 2 - Write a while loop that runs
-// 3 times. Each iteration will decrease
-// the value of checkz by -2
+// 2 - Write a while loop that runs 3 times. Each iteration will decrease the value of checkz by -2
 
-// 3 - Summarize in English what this code is doing.
-// Use a multiline comment to do so.
-//
+for (var i = 0; i < 3; i++) {
+  checkz -= 1;
+}
+
+
+// 3 - Summarize in English what this code is doing. Use a multiline comment to do so.
 var numbers = [1, 2, 3, 4, 5, 6, 7];
 var total = 0;
 for (var i = 0; i < numbers.length; i++) {
   total += numbers[i];
 }
 console.log(total);
+
+//This loop takes in this numbers array. While i is less than the length of the array,
+//the code loops through, and then total is increased by the value of the array at the
+//indicated index. Then the console.log prints the final value of total.
+//
